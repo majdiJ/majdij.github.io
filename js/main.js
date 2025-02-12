6e52cb21a8891c6b16f58383d2d49238ec14ef38
majdiJ <majdijaigirdar2@gmail.com>
2025-02-12 20:09:21 +0000

    Improved mobile view for menus, better privacy notices, personal message, better docuent view support

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function opdnHamburgerMenu() {
+  const navBar = document.querySelector('.nav-bar');
+  navBar.classList.add('active');
+}
+
+function closeHamburgerMenu() {
+  const navBar = document.querySelector('.nav-bar');
+  navBar.classList.remove('active');
+}
